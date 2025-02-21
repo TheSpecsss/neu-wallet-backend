12337e0405612deb201fe0483f8caaef6f68dd94
KuroXI <vgonzales5157@gmail.com>
2025-02-21 20:18:37 +0800

    allow cors and only accept POST method

--- src/shared/infrastructure/http/app.ts
+++ src/shared/infrastructure/http/app.ts
@@ -4,6 +4,7 @@ import { schema } from "@/shared/infrastructure/http/graphql/schema";
 import { type Context, authorize } from "@/shared/infrastructure/http/helpers/authorize";
 import { ApolloServer } from "@apollo/server";
 import fastifyApollo, { fastifyApolloDrainPlugin } from "@as-integrations/fastify";
+import fastifyCors from "@fastify/cors";
 import Fastify from "fastify";
 
 (async () => {
@@ -19,6 +20,8 @@ import Fastify from "fastify";
 	await fastify.register(fastifyApollo(apollo), {
 		context: authorize,
 	});
+
+	await fastify.register(fastifyCors, { origin: "*", methods: ["POST"] });
 
 	fastify.listen({ port: Number(process.env.PORT) || 8080 }, (error) => {
 		if (error) throw error;
